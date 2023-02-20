d2edb5846da1686922ba82fa08db609f6400d162
yangweiqi <phpcn@hotmail.com>
2023-02-20 22:10:48 +0800

    Update redis_keys.go

--- redis_keys.go
+++ redis_keys.go
@@ -1,15 +1,15 @@
 package rmq
 
 const (
-	connectionsKey                   = "rmq::connections"                                           // Set of connection names
-	connectionHeartbeatTemplate      = "rmq::connection::{connection}::heartbeat"                   // expires after {connection} died
-	connectionQueuesTemplate         = "rmq::connection::{connection}::queues"                      // Set of queues consumers of {connection} are consuming
-	connectionQueueConsumersTemplate = "rmq::connection::{connection}::queue::[{queue}]::consumers" // Set of all consumers from {connection} consuming from {queue}
-	connectionQueueUnackedTemplate   = "rmq::connection::{connection}::queue::[{queue}]::unacked"   // List of deliveries consumers of {connection} are currently consuming
+	connectionsKey                   = "{weihouqin_905}rmq::connections"                                           // Set of connection names
+	connectionHeartbeatTemplate      = "{weihouqin_905}rmq::connection::{connection}::heartbeat"                   // expires after {connection} died
+	connectionQueuesTemplate         = "{weihouqin_905}rmq::connection::{connection}::queues"                      // Set of queues consumers of {connection} are consuming
+	connectionQueueConsumersTemplate = "{weihouqin_905}rmq::connection::{connection}::queue::[{queue}]::consumers" // Set of all consumers from {connection} consuming from {queue}
+	connectionQueueUnackedTemplate   = "{weihouqin_905}rmq::connection::{connection}::queue::[{queue}]::unacked"   // List of deliveries consumers of {connection} are currently consuming
 
-	queuesKey             = "rmq::queues"                     // Set of all open queues
-	queueReadyTemplate    = "rmq::queue::[{queue}]::ready"    // List of deliveries in that {queue} (right is first and oldest, left is last and youngest)
-	queueRejectedTemplate = "rmq::queue::[{queue}]::rejected" // List of rejected deliveries from that {queue}
+	queuesKey             = "{weihouqin_905}rmq::queues"                     // Set of all open queues
+	queueReadyTemplate    = "{weihouqin_905}rmq::queue::[{queue}]::ready"    // List of deliveries in that {queue} (right is first and oldest, left is last and youngest)
+	queueRejectedTemplate = "{weihouqin_905}rmq::queue::[{queue}]::rejected" // List of rejected deliveries from that {queue}
 
 	phConnection = "{connection}" // connection name
 	phQueue      = "{queue}"      // queue name
